6275e74d6bc45567a0680cae04ef581144564b17
Heitor Kendy Hayashi Sato <104914588+HeitorKHS@users.noreply.github.com>
2023-08-23 09:08:55 -0300

    Add files via upload

--- review/Exercise5/calculator.php
+++ review/Exercise5/calculator.php
@@ -9,12 +9,40 @@ function calculatorParcel($percentage, $parcel, $financedAmount)
     return round($valueParcel, 2);
 }
 
-if(!empty($_POST['percentage']) && !empty($_POST['parcel']) && !empty($_POST['financedAmount']))
+function calculatorAll($valueParcel, &$saldoDevedor, $percentage, $i, &$totalJuros)
 {
-    $valueParcel = calculatorParcel($_POST['percentage'], $_POST['parcel'], $_POST['financedAmount']);
+    $list2 = [];
+
+    $juros = round(($saldoDevedor * $percentage)/100, 2);
+    $amount = round(($valueParcel - $juros), 2);
+    $saldoDevedor = round(($saldoDevedor - $amount), 2);
+    $totalJuros += $juros;
+    array_push($list2,$i+1);
+    array_push($list2,$valueParcel);
+    array_push($list2,$amount);
+    array_push($list2,$juros);
+    array_push($list2,max($saldoDevedor,0));
+    return $list2;
 }
 
-$_SESSION['result'] = $valueParcel;
+
+
+if(!empty($_POST['percentage']) && !empty($_POST['parcel']) && !empty($_POST['financedAmount']))
+{
+    $list = [];
+    $saldoDevedor = $_POST['financedAmount'];
+    $percentage = $_POST['percentage'];
+    $parcel = $_POST['parcel'];
+    $financedAmount = $_POST['financedAmount'];
+    $totalJuros = 0;
+
+    $valueParcel = calculatorParcel($percentage, $parcel, $financedAmount);
+    for($i=0; $i < $_POST['parcel']; $i++)
+        array_push($list, calculatorAll($valueParcel, $saldoDevedor, $percentage, $i, $totalJuros));
+}
+
+$_SESSION['result'] = $list;
+$_SESSION['juros'] = $totalJuros;
 header('location:./index.php');
 die();
 
--- review/Exercise5/index.php
+++ review/Exercise5/index.php
@@ -12,18 +12,43 @@ session_start();
     <title>Document</title>
 </head>
 <body>
+    
     <form method="POST" action="./calculator.php">
         <label for="financedAmount">Financed Amount: </label>
         <input type="number" name="financedAmount">
         <label for="percentage">Interest percentage: </label>
-        <input type="number" name="percentage">
+        <input type="number" name="percentage" step="0.1">
         <label for="parcel">Parcel: </label>
         <input type="number" name="parcel">
         <input type="submit" value="calculator">
     </form>
-    <?php
-
-    ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Parcela</th>
+                <th>Vlr Parcela</th>
+                <th>Amortização</th>
+                <th>Juros</th>
+                <th>Sdo devedor</th>
+            </tr>
+        </thead>
+    <tbody>
+        <?php
+            if(isset($_SESSION['result']))
+            {
+                foreach($_SESSION['result'] as $row)
+                {
+                    echo '<tr>';
+                    foreach($row as $col)
+                        echo '<td>'.$col.'</td>';
+                    echo '</tr>';
+                }
+                echo "<tr><td>Total: ".$_SESSION['juros']."</td></tr>";
+            }
+        ?> 
+    </tbody>
+    </table>
+    
 
 </body>
 </html>
